cc81e317350d5fd35faec646dbcf630dd4514188
AracelyCondoriTipola <aracely96.ac>
2022-10-04 23:34:33 -0400

    modoficando main adminr

--- src/main/main.component.spec.ts
+++ src/main/main.component.spec.ts
@@ -6,20 +6,4 @@ describe('MainComponent', () => {
   let component: MainComponent;
   let fixture: ComponentFixture<MainComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ MainComponent ]
-    })
-    .compileComponents();
-  });
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(MainComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+ 

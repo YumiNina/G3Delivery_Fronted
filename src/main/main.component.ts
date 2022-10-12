1058fe2a73925de7633fb3a4477098845de4d09e
AracelyCondoriTipola <aracely96.ac>
2022-10-12 08:49:08 -0400

    agregando buscador ususario repartidor

--- src/main/main.component.ts
+++ src/main/main.component.ts
@@ -4,4 +4,12 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.css']
+})
+export class MainComponent implements OnInit {
 
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
